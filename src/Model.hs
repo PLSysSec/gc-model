98d6d11e5a45a855d65ab34dc6bee03a489f48ef
Peter Amidon <peter@picnicpark.org>
2017-07-11 10:12:06 -0700

    the beginnings of a coherent model architecture

--- src/Model.hs
+++ src/Model.hs
@@ -12,9 +12,10 @@ import Control.Monad
 import Control.Monad.Except
 import Control.Monad.State.Lazy
 import Control.Monad.Trans
+import Control.Concurrent
 import Data.Array.IO
 import Data.Array.MArray
-import Data.Binary
+--import Data.Binary hiding (get, put)
 import Data.Bits
 import Data.Char
 import Data.IORef
@@ -30,6 +31,11 @@ import Data.Primitive.ByteArray
 import Test.QuickCheck hiding ((.&.))
 import Numeric
 
+import Control.Monad.ST
+import Data.Function
+import Data.List
+import Data.Ord
+
 -- fit in word64
 -- top 32 bits is metadata, bottom 32 bits is data
 --   - data can be 16 bit pointer, in which case high-order 16 bits must be 0
@@ -39,8 +45,8 @@ import Numeric
 --
 -- invariant:
 
---data Data      = Int Word32 | Pointer Word16
-data Type = Int | Pointer deriving (Show, Eq)
+--data Data      = Int Word32 | Ptr Word16
+data Type = Int | Ptr deriving (Show, Eq)
 data HeapEntry = HeapEntry Word16 -- ^ forwarding pointer
                            Type   -- ^ type
                            Bool   -- ^ GC bit
@@ -53,30 +59,30 @@ serialize (HeapEntry f t g d) =  ((shift (fromIntegral f)             48)
                              .|. (shift (fromIntegral otherMetadata) 32))
                              .|. fromIntegral data'
   where data' | t == Int     = d
-              | t == Pointer = d .&. 0xffff
+              | t == Ptr = d .&. 0xffff
         otherMetadata :: Word16
         otherMetadata =  shift (fromIntegral typeRepr) 1
                      .|. shift (fromIntegral gcRepr)   0
         typeRepr :: Word16
         typeRepr | t == Int     = 0
-                 | t == Pointer = 1
+                 | t == Ptr = 1
         gcRepr :: Word16
         gcRepr | g == False = 0
                | g == True = 1
 deserialize :: Word64 -> HeapEntry
 deserialize w = HeapEntry f t g d
   where f = fromIntegral $ shift w (-48)
-        t = if w .&. 0x200000000 > 0 then Pointer else Int
+        t = if w .&. 0x200000000 > 0 then Ptr else Int
         g = if w .&. 0x100000000 > 0 then True else False
         d = fromIntegral $ w .&. (case t of
                                     Int -> 0xffffffff
-                                    Pointer -> 0xffff)
+                                    Ptr -> 0xffff)
 
 p :: (Show a, Integral a) => a -> String
 p = flip showHex ""
 
 instance Arbitrary Type where
-  arbitrary = elements [Int, Pointer]
+  arbitrary = elements [Int, Ptr]
 
 instance Arbitrary HeapEntry where
   arbitrary = do
@@ -85,17 +91,117 @@ instance Arbitrary HeapEntry where
     g <- arbitrary
     d <- case t of
       Int -> arbitrary @Word32
-      Pointer -> fromIntegral <$> arbitrary @Word16
+      Ptr -> fromIntegral <$> arbitrary @Word16
     return $ (HeapEntry f t g d)
 
 prop_serialize_bidirectional :: HeapEntry -> Bool
 prop_serialize_bidirectional h = h == (deserialize . serialize) h
 
+type HeapPtr = Word16
+{-data Thread = Thread HeapPtr -- ^ The start of its free blocks list
+                     HeapPtr -- ^ The bottom of its stack
+                     HeapPtr -- ^ The top of its stack-}
+type Thread = Bool -- ^ is this the 1st thread or the 2nd
+
+--invariant for global free blocks list:
+--
+--  IOref of some integer
+--  every number preceding it is filled
+--
+-- actually there will be no free blocks list for now.
+
+-- each block is 1K
+
+-- PSA suggestion on simple thing to start with
+--
+-- we have only two thread
+-- each thread has its own blocks list
+-- each thread "owns" half of memory
+--
+-- thread 1 can put whatever it wants inside 0x0000 to 0x7FFF
+-- thread 2 can put whatever it wants inside 0x8000 to 0xFFFF
+
+
+type RTS m = StateT (Heap m) m
+
+runRTS :: Monad m => Heap m -> RTS m a -> m a
+runRTS = flip evalStateT
+
+{-runOneThread' :: forall s. RTS (ST s) ()
+runOneThread' = do
+  undefined
+
+runOneThread :: forall s. Heap (ST s) -> ST s ()
+runOneThread s = runRTS s runOneThread'
+
+main :: IO ()
+main = do
+  heap' <- newByteArray (8 * 0xFFFF)
+  let heap = Heap { heapMem = heap' }
+  forkIO $ runST $ runOneThread heap
+  forkIO $ runST $ runOneThread heap-}
+
+{-threadBase :: Thread -> HeapPtr
+threadBase False = 0x0000
+threadBase True  = 0x8000
+
+setupThread :: HeapPtr -> RTS IO ()
+setupThread threadBase = do
+  undefined-}
+
+fullHeap :: RTS IO Bool
+fullHeap = null . unFreeList <$> gets freeList
+
+splitLowestByte :: FreeList -> (HeapPtr, FreeList)
+splitLowestByte (FreeList f) = (el, FreeList new)
+  where (Interval el eh:es) = sortBy (compare `on` iLow) $ f
+        el' = el + 1
+        new | el' == eh = es
+            | otherwise = Interval el' eh:es
+
+alloc :: HeapEntry -> RTS IO ()
+alloc e = do
+  f <- fullHeap
+  if f then gcThisThread else do
+    h <- get
+    let (addr, f') = splitLowestByte (freeList h)
+    writeByteArray (heapMem h) (fromIntegral addr) (serialize e)
+    put h { freeList = f' }
+
+gcThisThread :: RTS IO ()
+gcThisThread = undefined
+
+runOneThread :: Bool -> RTS IO ()
+runOneThread t = do
+  undefined
+
+main :: IO ()
+main = do
+  heap' <- newByteArray (8 * 0xFFFF)
+  forkOS $ evalStateT (runOneThread False) $
+    Heap { heapMem = heap'
+         , heapRootSet = []
+         , freeList = FreeList [Interval 0x0001 0x7FFF ]
+         }
+  forkOS $ evalStateT (runOneThread True) $
+    Heap { heapMem = heap'
+         , heapRootSet = []
+         , freeList = FreeList [Interval 0x8001 0xFFFF ]
+         }
+  forever $ threadDelay 100000000000
+
+
 -- other structures to consider:
 --   rose tree of IORefs / Ptr Word32
 
+data Interval = Interval { iLow :: HeapPtr, iHigh :: HeapPtr }
+newtype FreeList = FreeList { unFreeList :: [Interval] }
+
 data Heap m = Heap
-  { heapMem :: MutableByteArray (PrimState m) -- this is NOT block structured!!!
+  -- this is NOT block structured!!!
+  { heapMem      :: MutableByteArray (PrimState m)
+  , heapRootSet :: [HeapPtr]
+  , freeList    :: FreeList
   -- , heapBounds :: (Int,Int)     -- array bounds
   -- , heapFree :: Int             -- next free index
   }
